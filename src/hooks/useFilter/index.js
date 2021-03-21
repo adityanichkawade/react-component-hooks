2a207ea3a92c4b7dbf974f2f6c5607292f10015c
adityanichkawade <aditya.nichkawade@gmail.com>
2021-03-21 21:31:10 +0530

    Corrected variables name

--- src/hooks/useFilter/index.js
+++ src/hooks/useFilter/index.js
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
 
 function useFilter(data) {
-  const [filteredData, setFilteredData] = useState(data);
+  const [filterResult, setFilterResult] = useState(data);
   const [filterConfig, setFilterConfig] = useState({});
 
   const { searchKey, searchValue } = filterConfig;
 
   useEffect(() => {
-    setFilteredData(data.filter((item) => item[searchKey] === searchValue));
+    setFilterResult(
+      data.filter((item) =>
+        searchKey ? item[searchKey] === searchValue : item === searchValue
+      )
+    );
   }, [data, searchValue, searchKey]);
 
   const applyFilter = (config) => setFilterConfig(config);
 
-  return { filteredData, applyFilter };
+  return { filterResult, applyFilter };
 }
 
 export default useFilter;
